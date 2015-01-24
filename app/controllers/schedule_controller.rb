af75848bc4cc3a9c9c4edc8fe7d5e70fbe3a3f2b
SherLock Tuan <minhtuan0602@gmail.com>
2015-01-25 02:59:37 +0700

    sketch for hopital insert

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,5 +1,27 @@
 class ScheduleController < ApplicationController
   def user_input
   	@message = "Hello, how are you today?"
+  	puts @people.collect {|p| [ p.name, p.id ]}
+  end
+
+  def check_user_input
+  	@message1 = params[:q]
+  	puts @message1
+  	puts "----------------------"
+  	render 'user_input'
+  end
+
+  def create_db_hopital
+  	Hopital.create(name: "David", address: "Code Artist", district: "")
+  	Hopital.create(name: "David", address: "Code Artist", district: "")
+  	Hopital.create(name: "David", address: "Code Artist", district: "")
+  	Hopital.create(name: "David", address: "Code Artist", district: "")
+  	Hopital.create(name: "David", address: "Code Artist", district: "")
+  	Hopital.create(name: "David", address: "Code Artist", district: "")
+  	Hopital.create(name: "David", address: "Code Artist", district: "")
+  	Hopital.create(name: "David", address: "Code Artist", district: "")
+  	Hopital.create(name: "David", address: "Code Artist", district: "")
+  	Hopital.create(name: "David", address: "Code Artist", district: "")
+  	Hopital.create(name: "David", address: "Code Artist", district: "")
   end
 end
