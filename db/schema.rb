3c0174c411fd95d2e69d38428d363fbd91f9765c
SherLock Tuan <minhtuan0602@gmail.com>
2015-01-25 03:19:12 +0700

    prepare for appointment

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150124194738) do
+
+  create_table "hopitals", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "district"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
